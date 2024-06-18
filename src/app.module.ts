b1b3d206008aa40ce1df81abc324bc158851ce62
erik-martins <erik.ozzetti.martins@gmail.com>
2024-06-18 13:09:04 -0300

    feat: typeorm config migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from './config/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development', '.env.production'],
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      // useFactory: () => ({
+      //   type: 'postgres',
+      //   host: process.env.DB_HOST,
+      //   port: parseInt(process.env.DB_PORT, 10),
+      //   username: process.env.DB_USER,
+      //   password: process.env.DB_PASS,
+      //   database: process.env.DB_NAME,
+      //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      //   migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      //   migrationsRun: true,
+      //   synchronize: false,
+      // }),
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
